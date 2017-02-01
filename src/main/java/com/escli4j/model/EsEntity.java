f7145dfc946b3fec3b20735578f87e9bac9eb570
gkozyryatskyy <g.kozyryatskyy@gmail.com>
2017-02-01 16:00:58 +0200

    json read

--- src/main/java/com/escli4j/model/EsEntity.java
+++ src/main/java/com/escli4j/model/EsEntity.java
@@ -1,8 +1,12 @@
 package com.escli4j.model;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-public class EsEntity {
+public class EsEntity implements Serializable {
+
+    private static final long serialVersionUID = 3157546209065026044L;
 
     @JsonIgnore
     protected String id;
