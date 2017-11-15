28311792732a7fb58b8cc9c2cc11fdc428380329
gleb.kozyryatskyy <gleb.kozyryatskyy@192.168.198.88>
2017-11-15 16:28:36 +0200

    main test fix

--- src/main/java/com/escli4j/MainTest.java
+++ src/main/java/com/escli4j/MainTest.java
@@ -8,41 +8,22 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
-import com.escli4j.annotations.Type;
-import com.escli4j.dao.ChildEntityDao;
-import com.escli4j.model.EsChildEntity;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.escli4j.mapping.Mapping;
 
 public class MainTest {
 
-    @SuppressWarnings("resource")
+    @SuppressWarnings({ "resource", "unused" })
     public static void main(String[] args) throws IOException {
         try {
             Client esClient = new PreBuiltTransportClient(Settings.EMPTY)
                     .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
             System.out.println("started");
-            // Mapping mapping = new Mapping(esClient);
+            Mapping mapping = new Mapping(esClient);
             // mapping.migrate();
-            TestDao dao = new TestDao(esClient);
-            System.out.println(dao.get("AV80Qyj3wN7gDfKCr20h", "qqwerqwerqwerwqer"));
             System.out.println("done");
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @Type(index = "tt", type = "clip", parent = "video", update = true)
-    public static class TestObject extends EsChildEntity {
-
-    }
-
-    public static class TestDao extends ChildEntityDao<TestObject> {
-
-        public TestDao(Client client) {
-            super(TestObject.class, client);
-        }
-
     }
 
 }
